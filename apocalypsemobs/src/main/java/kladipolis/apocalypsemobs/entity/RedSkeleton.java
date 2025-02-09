1463ddbfa281231db10190725324516d6880b341
osariusz <osariusz@interia.pl>
2025-02-09 15:35:43 +0100

    Pestilence intro

--- apocalypsemobs/src/main/java/kladipolis/apocalypsemobs/entity/RedSkeleton.java
+++ apocalypsemobs/src/main/java/kladipolis/apocalypsemobs/entity/RedSkeleton.java
@@ -8,34 +8,33 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.animal.*;
 import net.minecraft.world.entity.monster.*;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.neoforged.neoforge.event.EventHooks;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
-public class RedSkeleton extends Monster {
-    //work in progress pestilence
-    private static final int TOTAL_CONVERSION_TIME = 300;
-    private static final EntityDataAccessor<Boolean> DATA_STRAY_CONVERSION_ID;
-    public static final String CONVERSION_TAG = "StrayConversionTime";
-    private int inPowderSnowTime;
-    private int conversionTime;
-
+public class RedSkeleton extends PathfinderMob {
+    //TODO: Disable fall damage, add farmland replacement and progress bars for goals
     public final List<Class<? extends LivingEntity>> POISONABLE_MOBS = Arrays.asList(
             Player.class,
             EntityCitizen.class,
@@ -51,25 +50,17 @@ public class RedSkeleton extends Monster {
         System.out.println("eeelo");
     }
 
+    protected void registerGoals() {
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, (double)1.0F, false));
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this, new Class[0]));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal(this, Player.class, true));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, (double)1.0F));
+        //TODO: Find farmland and poison goals
+        this.goalSelector.getAvailableGoals();
+    }
+
     public static AttributeSupplier.Builder createAttributes() {
         return Monster.createMonsterAttributes().add(Attributes.MOVEMENT_SPEED, (double)0.25F);
-    }
-
-    protected void defineSynchedData(SynchedEntityData.Builder builder) {
-        super.defineSynchedData(builder);
-        builder.define(DATA_STRAY_CONVERSION_ID, false);
-    }
-
-    public boolean isFreezeConverting() {
-        return (Boolean)this.getEntityData().get(DATA_STRAY_CONVERSION_ID);
-    }
-
-    public void setFreezeConverting(boolean isFrozen) {
-        this.entityData.set(DATA_STRAY_CONVERSION_ID, isFrozen);
-    }
-
-    public boolean isShaking() {
-        return this.isFreezeConverting();
     }
 
     public List<? extends LivingEntity> getNearbyPoisonableEntities() {
@@ -98,63 +89,11 @@ public class RedSkeleton extends Monster {
     }
 
     public void tick() {
+        System.out.println(this.goalSelector.getAvailableGoals());
         if (!this.level().isClientSide && this.isAlive() && !this.isNoAi()) {
-            if (this.isInPowderSnow) {
-                if (this.isFreezeConverting()) {
-                    --this.conversionTime;
-                    if (this.conversionTime < 0) {
-                        this.doFreezeConversion();
-                    }
-                } else {
-                    ++this.inPowderSnowTime;
-                    if (this.inPowderSnowTime >= 140) {
-                        this.startFreezeConversion(300);
-                    }
-                }
-            } else {
-                this.inPowderSnowTime = -1;
-                this.setFreezeConverting(false);
-            }
             applyPoisonNearby();
         }
-
         super.tick();
-    }
-
-    public void addAdditionalSaveData(CompoundTag compound) {
-        super.addAdditionalSaveData(compound);
-        compound.putInt("StrayConversionTime", this.isFreezeConverting() ? this.conversionTime : -1);
-    }
-
-    public void readAdditionalSaveData(CompoundTag compound) {
-        super.readAdditionalSaveData(compound);
-        if (compound.contains("StrayConversionTime", 99) && compound.getInt("StrayConversionTime") > -1) {
-            this.startFreezeConversion(compound.getInt("StrayConversionTime"));
-        }
-
-    }
-
-    private void startFreezeConversion(int conversionTime) {
-        this.conversionTime = conversionTime;
-        this.setFreezeConverting(true);
-    }
-
-    protected void doFreezeConversion() {
-        if (EventHooks.canLivingConvert(this, EntityType.STRAY, (timer) -> this.conversionTime = timer)) {
-            Stray stray = (Stray)this.convertTo(EntityType.STRAY, true);
-            if (stray != null) {
-                EventHooks.onLivingConvert(this, stray);
-            }
-
-            if (!this.isSilent()) {
-                this.level().levelEvent((Player)null, 1048, this.blockPosition(), 0);
-            }
-
-        }
-    }
-
-    public boolean canFreeze() {
-        return false;
     }
 
     protected SoundEvent getAmbientSound() {
@@ -172,21 +111,4 @@ public class RedSkeleton extends Monster {
     protected SoundEvent getStepSound() {
         return SoundEvents.SKELETON_STEP;
     }
-
-    protected void dropCustomDeathLoot(ServerLevel level, DamageSource damageSource, boolean recentlyHit) {
-        super.dropCustomDeathLoot(level, damageSource, recentlyHit);
-        Entity var5 = damageSource.getEntity();
-        if (var5 instanceof Creeper creeper) {
-            if (creeper.canDropMobsSkull()) {
-                creeper.increaseDroppedSkulls();
-                this.spawnAtLocation(Items.SKELETON_SKULL);
-            }
-        }
-
-    }
-
-    static {
-        DATA_STRAY_CONVERSION_ID = SynchedEntityData.defineId(RedSkeleton.class, EntityDataSerializers.BOOLEAN);
-    }
-
 }
