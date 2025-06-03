5fcd757c660d70e0203618e053d6169c84797325
osariusz <osariusz@interia.pl>
2025-06-03 22:33:49 +0200

    Add replacement logic

--- kladipolis-util/src/main/java/com/makrowave/kladipolis_util/Mixins/MultiNoiseBiomeSourceMixin.java
+++ kladipolis-util/src/main/java/com/makrowave/kladipolis_util/Mixins/MultiNoiseBiomeSourceMixin.java
@@ -2,6 +2,7 @@ package com.makrowave.kladipolis_util.Mixins;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
@@ -12,14 +13,38 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(MultiNoiseBiomeSource.class)
 public class MultiNoiseBiomeSourceMixin {
+    private static final int COLD_BIOME_THRESHOLD = 0;
+    private static final int BIOME_PROBE_Y = 100;
+    private static final int BIOME_FORCE_MAX = 64;
+
+    List<ResourceKey<Biome>> EXCEPTIONS = new ArrayList<>(List.of(
+            Biomes.DEEP_DARK
+    ));
+
+    private boolean isForcedBiome(Holder<Biome> biome) {
+        return biome.value().getModifiedClimateSettings().temperature() <= COLD_BIOME_THRESHOLD || biome.is(Biomes.RIVER);
+    }
+
+    private boolean isExceptionBiome(Holder<Biome> biome) {
+        return EXCEPTIONS.stream().anyMatch(
+                (resourceKey) -> resourceKey.equals(
+                        biome.unwrapKey().orElse(null)
+                )
+        );
+    }
+
     @Inject(method = "getNoiseBiome", at = @At("TAIL"), cancellable = true)
     private void modifyBiome(int x, int y, int z, Climate.Sampler sampler, CallbackInfoReturnable<Holder<Biome>> cir) {
-        if(y < 64) {
+        if(y < BIOME_FORCE_MAX) {
             MultiNoiseBiomeSource source = (MultiNoiseBiomeSource)(Object)this;
-            Holder<Biome> biomeAt100 = source.getNoiseBiome(x, 100, z, sampler);
-            if (biomeAt100.is(Biomes.TAIGA) && y < 100) {
+            Holder<Biome> biomeAt100 = source.getNoiseBiome(x, BIOME_PROBE_Y, z, sampler);
+            Holder<Biome> originalBiome = cir.getReturnValue();
+            if (isForcedBiome(biomeAt100) && !isExceptionBiome(originalBiome)) {
                 cir.setReturnValue(biomeAt100);
             }
         }
